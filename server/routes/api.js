7a163cbb5206c978cb5a2503fdc9cc6239363fd3
fedi.gmd <gammoudifedi@gmail.com>
2021-01-20 22:47:27 +0100

    add login and register apis

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const User = require('../models/user')
 const mongoose = require('mongoose')
 const db = 'mongodb+srv://fedigmd:Az11610141@cluster0.gisjg.mongodb.net/Netflix_app?retryWrites=true&w=majority'
 
@@ -21,6 +21,40 @@ mongoose.connect(db, connectionParams)
 
 router.get('/', (req, res) => {
   res.send('From Api route')
+})
+
+router.post('/register', (req, res) => {
+  let userData = req.body
+  console.log('userData', userData);
+  let user = new User(userData)
+  user.save((error, registeredUser) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.status(200).send(registeredUser)
+    }
+  })
+})
+
+router.post('/login', (req, res) => {
+  let userData = req.body
+  console.log('userData', userData);
+
+  User.findOne({ email: userData.email }, (error, user) => {
+    if (error) {
+      console.log('errorerrorerrorerrorerrorerrorerror',error);
+    } else {
+      if (!user) {
+        res.status(401).send('Invalid email')
+      } else {
+        if (user.password !== userData.password) {
+          res.status(401).send('Invalid password')
+        } else {
+          res.status(200).send(user)
+        }
+      }
+    }
+  })
 })
 
 module.exports = router
