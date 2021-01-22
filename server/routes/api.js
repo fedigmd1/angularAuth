893c8264f7ca231b2ca9b8bd31dc0926df339313
fedi.gmd <gammoudifedi@gmail.com>
2021-01-22 19:07:17 +0100

    add jwt token generator

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const router = express.Router()
 const User = require('../models/user')
 const mongoose = require('mongoose')
@@ -37,7 +38,9 @@ router.post('/register', (req, res) => {
           if (error) {
             console.log(error);
           } else {
-            res.status(200).send(registeredUser)
+            let payload = { subject: registeredUser._id }
+            let token = jwt.sign(payload, 'secretkey')
+            res.status(200).send({ token })
             console.log('user added with success');
           }
         })
@@ -65,7 +68,9 @@ router.post('/login', (req, res) => {
           res.status(401).send('Invalid password')
           console.log('Invalid password');
         } else {
-          res.status(200).send(user)
+          let payload = { subject: user._id }
+          let token = jwt.sign(payload, 'secretkey')
+          res.status(200).send({ token })
           console.log('Login with Success!');
         }
       }
